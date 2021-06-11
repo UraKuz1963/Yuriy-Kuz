96fb0ed23c453b6a3f04ec8bb5a42c0e66535a73
Yuriy-Kuz <urakuz@yandex.ru>
2021-06-11 17:04:06 +0500

    the end2

---
+++ PrimStr.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+func Contains(a string, x string) bool {
+	for isimv := 0; isimv < len(a); isimv++ {
+		for i := 0; i < len(x); i++ {
+			if x[i] != a[isimv+i] {
+				break
+			}
+			if i == len(x)-1 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func seekinBody(host string, strFind string, c chan string) bool {
+	resp, err := http.Get(host)
+	if err != nil {
+		fmt.Println(err)
+		<-c
+		return false
+	}
+	defer resp.Body.Close()
+	strBody := ""
+	for true {
+		bs := make([]byte, 32768)
+		n, err := resp.Body.Read(bs)
+		strBody += string(bs[:n])
+		if n == 0 || err != nil {
+			if Contains(strBody, strFind) == true {
+				fmt.Println("В хосте ", host, " найдена строка ", strFind, "\n")
+				<-c
+				return true
+			}
+			break
+		}
+	}
+	<-c
+	return false
+}
+
+func main() {
+	numVorkers := flag.Int("numVorkers", 3, "an int")
+	flag.Parse()
+	//iRut := 0
+	f, err := os.Open("hello.txt")
+	check(err)
+	fmt.Println("Start: ", *numVorkers, "\n")
+	if err != nil { // если возникла ошибка
+		fmt.Println("Unable to open file:", err)
+		os.Exit(1) // выходим из программы
+	}
+	b1 := make([]byte, 1024) //Предполагаем пока макс длинну файла 1024 байт
+	n1, err := f.Read(b1)
+	check(err)
+	words := strings.Fields(string(b1[:n1]))
+	var input string
+	fmt.Println("\n", "Введите строку для поиска:")
+	fmt.Scanln(&input)
+	fmt.Println("\n", "Идет поиск:", "\n")
+	iRut := make(chan string, *numVorkers)
+	for _, word := range words {
+		iRut <- "1" // ставим флаг в канал о запущеной горутине
+		go seekinBody(word, input, iRut)
+	}
+	iFlag := true
+	for iFlag {
+		select {
+		case <-iRut: // ожидание исчерпания буфера канала
+			time.Sleep(time.Millisecond * 10) // задержка 10мс
+		default:
+			iFlag = false
+		}
+	}
+	fmt.Println("End:", "\n")
+	defer f.Close()                       // закрываем файл
+	fmt.Println(" Имя файла: ", f.Name()) // hello.txt
+	return
+}
